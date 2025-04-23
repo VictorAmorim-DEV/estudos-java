4f65b437dd72e048dd6cab0a19de7fe56fe42f1c
Victor Amorim <dev.victoramorim@gmail.com>
2025-04-23 19:16:46 -0300

    exercicio_lista

---
+++ src/entites/EmployeeForList.java
@@ -0,0 +1,44 @@
+package entites;
+
+public class EmployeeForList {
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public EmployeeForList() {
+	}
+	
+	public EmployeeForList(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void increaseSalary(double percentage) {
+		salary += (salary * (percentage/100));
+	}
+	
+	public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
+	
+	
+	
+
+}
