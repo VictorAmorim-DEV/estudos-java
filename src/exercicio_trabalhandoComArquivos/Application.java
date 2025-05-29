1e48f70809f2f0418c995105a5c985107d4fee6c
Victor Amorim <dev.victoramorim@gmail.com>
2025-05-28 23:08:17 -0300

    exercicio_trabalhandoComArquivos

---
+++ src/exercicio_trabalhandoComArquivos/Application.java
@@ -0,0 +1,62 @@
+package exercicio_trabalhandoComArquivos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List <Product> list = new ArrayList<>();
+		
+		System.out.print("Enter file path:");
+		String designSourceFile = sc.nextLine();
+		
+		File sourceFile = new File(designSourceFile);
+		String sourceFolder = sourceFile.getParent();
+		
+		boolean sucess = new File(sourceFolder + "\\out").mkdir();
+		String targetFile = sourceFile + "\\out\\summary.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFolder))) {
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+				list.add(new Product(name, price, quantity));
+				line = br.readLine();
+			}
+		
+		
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))) {
+				for (Product item : list) {
+					bw.write(item.getName() + ", " +  item.getPrice());
+					bw.newLine();
+				}
+			}
+			catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+
+	}
+
+}
